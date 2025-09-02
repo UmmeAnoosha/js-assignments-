5615ce5f789394f4a2bf35a2e18d5d731c4fbce6
UmmeAnoosha <ummeanoosha08@gmail.com>
2025-09-02 09:14:16 -0700

    chapter1

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-// question:01" Write a script to greet your website visitor using JS alert box."
-// ans// 
-
+// question:01
+// alert("hello user")
 // question:02"
+// document.write("Error!Please enter a valid password")
+// question:03
+// alert("Welcome to JS land.. \n Happy Coding!")
+// question:04
+// alert("welcome to JS land")
+// alert("Happy Coding!")
+// question:05
+// console.log("Hello.... I can run JS through my web browser's console")
