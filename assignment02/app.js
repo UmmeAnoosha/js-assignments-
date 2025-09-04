edd18cf069dbcf0f40c09f8ed5a84bd82153518d
ghazalababar16 <g.babar.khan@gmail.com>
2025-09-04 12:32:15 +0500

    chapter02

---
+++ assignment02/app.js
@@ -0,0 +1,38 @@
+// question:01
+// var username 
+
+// question:02
+// var myName ="UmmeAnoosha"
+
+// question:03
+// var message
+// var message = "Hello world"
+// alert(message)
+
+//question:04
+// var studentName ="UmmeAnoosha"
+// var studentAge ="16 years old"
+// var studentCourse ="Web development"
+// alert( studentName)
+// alert(studentAge)
+// alert(studentCourse)
+
+// question:05
+// var text ="PIZZA \n PIZZ \n PIZ  \n PI \n P "
+// alert( text)
+
+// question:06
+// var email ="example@example.com"
+// alert("My email address is "+""+email)
+
+// question:07
+// var book ="A smarter way to learn JavaScript"
+// alert("I am trying to learn from Book "+book)
+
+// question:08
+// var content ="Yah!I can write HTML through Javascript."
+// document.write(content)
+
+// question:09
+// var random ="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(random)
