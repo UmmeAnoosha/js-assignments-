917363024d1e931954199d9d34649ebc346b036b
ghazalababar16 <g.babar.khan@gmail.com>
2025-09-04 13:15:53 +0500

    chapter3

---
+++ assignment03/app.js
@@ -0,0 +1,16 @@
+// question:01
+// var age ="16"
+// alert("I am "+age+" years old")
+
+// question:02
+// var number ="14"
+// alert("You have visited this site "+number+" times")
+
+// question:03
+// var birthYear ="2009"
+// alert("My birth year is "+birthYear)
+
+// question:04
+// var visitor ="Ayesha"
+// var quantity ="10"
+// alert(visitor+" orderd "+quantity+" tops on XYZ Clothing store”.")
