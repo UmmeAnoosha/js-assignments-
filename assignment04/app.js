78689419d452d25f248dc86b8d0989fc7771b516
UmmeAnoosha <ummeanoosha08@gmail.com>
2025-09-07 04:38:42 -0700

    done

---
+++ assignment04/app.js
@@ -0,0 +1,28 @@
+// question:01
+// let studentName,age,rollNumber;
+
+// question:02
+    //  LEGAL
+// let userName 
+// let userAge
+// let rollNumber
+// let $sellPrice 
+// let vallidEmail
+    // ILLEGAL
+// let 1stAssignment
+// let 1st-Assignment
+// let 1st Assignment 
+// let 1st#assifnment 
+// let const Assignment
+
+// question:03
+// (a)
+// document.write( "Rules for naming JS variables")
+// (b)
+// document.write("Variable names can only contain letters,$ and _.For example $my_1stVariable")
+// (c)
+// document.write("Variables9 must begin with a letters, $ or _. For example $name, _name or name")
+// (d)
+// document.write("Variable names are case sensitive")
+// (e)
+// document.write("Variable names should not be JS as keyword")
