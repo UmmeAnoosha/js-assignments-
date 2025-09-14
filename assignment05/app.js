1cc1a555be55774476403f2057462f55dcca9993
UmmeAnoosha <ummeanoosha08@gmail.com>
2025-09-14 12:38:22 -0700

    done

---
+++ assignment05/app.js
@@ -0,0 +1,73 @@
+// question:01
+// let number1 =3
+// let number2 =5
+// let sum = number1+number2
+// document.write(`sum of ${number1} and ${number2} is ${sum}`);
+
+// question:02
+    // (a)
+// let number1 =3
+// let number2 =5
+// let sub = number1-number2
+// document.write(`sub of ${number1} and ${number2} is ${sub}`);
+    //   (b)
+// let number1 =3 
+// let number2 =5
+// let multiple = number1*number2
+// document.write(`multiplication of ${number1} and ${number2} is ${multiple}`);
+    // (c)
+// let number1 =3
+// let number2 =5
+// let divide = number1/number2
+// document.write(`division of ${number1} and ${number2} is ${divide}`);
+    //  (d)
+// let number1 =3
+// let number2 =5
+// let modulus = number1%number2
+// document.write(`modulus of ${number1} and ${number2} is ${modulus}`);
+
+// question:03
+// let valueName ="Value after variable declaration is:"
+// document.write (`${valueName}<br>`)
+// let number =5
+// document.write(`Initial value:${number}<br>`)
+// let number1 =1
+// let valueIncrement = number+ number1
+// document.write(`Value after increment is:${valueIncrement}<br>`)
+// let number2 =7
+// let addingValue = valueIncrement+number2
+// document.write(`Value after adding is:${addingValue}<br>`)
+// let decrementValue = addingValue-number1
+// document.write(`Value after decrement is:${decrementValue}<br>`)
+// let divideValue =number1/3
+// document.write(`The remainder is: ${divideValue}`)
+
+
+
+// question:04
+// let movieTicket = 600 
+// let allTickets = 5
+// let multiple=movieTicket*allTickets
+// document.write(`Total cost of buying 5 tickets to a movie is ${multiple}PKR`)
+
+// question:05
+// let num1=4
+// document.write(`4x1=${num1}<br>`)
+// let num2=8
+// document.write(`4x2=${num2}<br>`)
+// let num3=12
+// document.write(`4x3=${num3}<br>`)
+// let num4=16
+// document.write(`4x4=${num4}<br>`)
+// let num5=20
+// document.write(`4x5=${num5}<br>`)
+// let num6=24
+// document.write(`4x6=${num6}<br>`)
+// let num7=28
+// document.write(`4x7=${num7}<br>`)
+// let num8=42
+// document.write(`4x8=${num8}<br>`)
+// let num9=46
+// document.write(`4x9=${num9}<br>`)
+// let num10=40
+// document.write(`4x10=${num10}<br>`)
