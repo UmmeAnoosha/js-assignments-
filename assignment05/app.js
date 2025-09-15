af1cadfec5b072a0163e8cde65955d915665948c
UmmeAnoosha <ummeanoosha08@gmail.com>
2025-09-15 11:56:31 -0700

    done

--- assignment05/app.js
+++ assignment05/app.js
@@ -42,8 +42,6 @@
 // let divideValue =number1/3
 // document.write(`The remainder is: ${divideValue}`)
 
-
-
 // question:04
 // let movieTicket = 600 
 // let allTickets = 5
@@ -71,3 +69,79 @@
 // document.write(`4x9=${num9}<br>`)
 // let num10=40
 // document.write(`4x10=${num10}<br>`)
+
+// question:06
+
+
+
+// question:07
+// let price1 =100
+// document.write(`Price of deal 1 is ${price1}<br>`)
+// let quantity1 =7
+// document.write(`Ordered quantity of deal 1 is ${quantity1}<br>`)
+// let price2 =650
+// document.write(`Price of deal 2 is ${price2}<br>`)
+// let quantity2 =3
+// document.write(`Ordered quantity of deal 2 is ${quantity2}<br>`)
+// let charges =100
+// document.write(`Shipping charges is ${charges} <br><br>`)
+// let claculate1 =price1*quantity1
+// let claculate2 =price2*quantity2
+// let total =claculate1+claculate2+charges
+// document.write(`Total cost of your order is ${total}`)
+
+//question:08
+// let totalMarks =980
+// document.write(`Total marks:${totalMarks}<br>`)
+// let obtainMarks =804
+// document.write(`Marks obtained :${obtainMarks}<br>`)
+// let number =100
+// let percentage =obtainMarks/totalMarks*number
+// document.write(`Percentage:${percentage}%`)
+
+// question:09
+// let usDoller =281.55
+// document.write(`1 USdoller =${usDoller} in Pakistani rupees <br>`)
+// let numberUsDoller =10
+// let claculate1 = usDoller* numberUsDoller
+// document.write(`${numberUsDoller} USdollers =${claculate1} in Pakistani rupees <br><br>`)
+// let riyal =75.06 
+// document.write(`1 SaudiRiyal =${riyal} in Pakistani rupees <br>`)
+// let numberRiyal =25
+// let claculate2 =riyal*numberRiyal
+// document.write(`${riyal} SaudiRiyal =${claculate2} in Pakistani rupees <br><br>`)
+// let claculate3 =claculate1+claculate2
+// document.write(`Total currency in PKR =${claculate3}`)
+
+// question:10
+// let number =8
+// let claculate =number+5*10/2
+// console.log(claculate);
+
+// question:11
+// let currentYear =2025
+// document.write(`Current year =${currentYear}<br>`)
+// let birthYear =2009
+// document.write(`Birth year =${birthYear} <br>`)
+// let age =currentYear-birthYear
+// document.write(`Now my age is ${age} old`)
+
+// question:12
+// let radius =20
+// document.write(`Radius of circle = ${radius}<br>`)
+// let circumference =2*3.142*radius
+// document.write(`The circumference = ${circumference}<br>`)
+// let areaCalculate = 3.142*(20*20)
+// document.write(`The total area = ${areaCalculate} `)
+
+// question"13
+// let favSnack = "Chocolate"
+// document.write(`Favourite Snack = ${favSnack}<br>`)
+// let currentAge =16
+// document.write(`Current age = ${currentAge}<br>`)
+// let maxAge =65
+// document.write(`Maximum age = ${maxAge}<br>`)
+// let snack =4
+// document.write(`Amount of snack per day = ${snack}<br>`)
+// let claculate = snack*maxAge
+// document.write(`You will need ${claculate} to last you until the ripe old age of ${currentAge}.`)
