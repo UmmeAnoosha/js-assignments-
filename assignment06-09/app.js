7a6814302b52b155355ebb019fbd0192a77b3e59
UmmeAnoosha <ummeanoosha08@gmail.com>
2025-09-18 09:54:20 -0700

    done

--- assignment06-09/app.js
+++ assignment06-09/app.js
@@ -36,3 +36,49 @@
 // }
 
 // questio:06
+// let sub1 =prompt("write your first subject")
+// let sub2 =prompt("write your second subject")
+// let sub3 =prompt("write your third subject")
+
+// let sub1Marks = 100
+// let sub2Marks = 100
+// let sub3Marks = 100
+
+// let sub1ObtainedMmarks =+prompt(`write your obtained marks of ${sub1}`)
+// let sub2ObtainedMmarks =+prompt(`write your obtained marks of ${sub2}`)
+// let sub3ObtainedMmarks =+prompt(`write your obtained marks of ${sub3}`)
+
+// let sub1Calculate = sub1ObtainedMmarks/sub1Marks*100
+// let sub2Calculate = sub2ObtainedMmarks/sub2Marks*100
+// let sub3Calculate = sub3ObtainedMmarks/sub3Marks*100
+
+// let sub1Percentage = sub1Calculate
+// let sub2Percentage = sub2Calculate
+// let sub3Percentage = sub3Calculate
+
+// document.write(`<table border>
+//         <tr>
+//             <th>Subject</th>
+//             <th>Totalmarks</th>
+//             <th>Obtainedmarks</th>
+//             <th>Percentage</th>
+//         </tr>
+//         <tr>
+//             <td>${sub1}</td>
+//             <td>${sub1Marks}</td>
+//             <td>${sub1ObtainedMmarks}</td>
+//             <td>${sub1Percentage}</td>
+//         </tr>
+//         <tr>
+//             <td>${sub2}</td>
+//             <td>${sub2Marks}</td>
+//             <td>${sub2ObtainedMmarks}</td>
+//             <td>${sub2Percentage}</td>
+//         </tr>
+//         <tr>
+//             <td>${sub3}</td>
+//             <td>${sub3Marks}</td>
+//             <td>${sub3ObtainedMmarks}</td>
+//             <td>${sub3Percentage}</td>
+//         </tr>
+//     </table>)`)
