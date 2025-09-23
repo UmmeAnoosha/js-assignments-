4c18e58f512124638722001a6e912d87a58631a7
UmmeAnoosha <ummeanoosha08@gmail.com>
2025-09-22 19:22:25 -0700

    done

---
+++ assignment09-11/app.js
@@ -0,0 +1,140 @@
+// question:01
+// let userCity =prompt("whats your city?")
+// if(userCity=="Karachi"){
+//     alert(`Welcome to city of lights`)
+// }
+
+// question:02
+// let userGender =prompt("whats your gender")
+// if (userGender=="Male"){
+//     alert(`Good Morning Sir`)
+// }
+// else if(userGender=="Female"){
+//     alert(`Good Morning Maam`)
+// }
+// else{
+//      alert(`you are not allowed`)
+// }
+
+// question:03
+// let userSignal =prompt("write any traffic signal color")
+// if (userSignal=="Red"){
+//     alert(`Must Stop`)
+// }
+// else if(userSignal=="Yellow"){
+//     alert(`Ready to move`)
+// }
+// else if(userSignal=="Green"){
+//     alert(`Move now`)
+// }
+// else{
+//     alert(`Your answer is wrong`)
+// }
+
+// question:04
+// let userFuel =+prompt("How many fuel remains in your car?")
+// if (userFuel<=0.25){
+//     alert(`Please refill the fuel in your car`)
+// }
+// else{
+//     alert(`your fuel is enough for car`)
+// }
+
+// question:05
+
+
+
+// question:06
+
+
+// question:07
+// let secretNumber =+prompt("write any number from 1 to 10")
+// if (secretNumber==5){
+//     alert(`Bingo! Correct answer`)
+// }
+// else if(secretNumber==4){
+//     alert(`Close enough to the correct answer`)
+// }
+// else {
+//     alert(`Ooops! you can not solved the puzzle`)
+// }
+
+// question:08
+// let userNumber =+prompt("write any number ")
+// if (userNumber==3){
+//     alert(`0`)
+// }
+// else{
+//     alert(`your answer is not match `)
+// }
+
+// question:09
+// let userNumber =+prompt("write any number from1-10")
+// if(userNumber==2){
+//     alert(`its even number`)
+// }
+// else if(userNumber==4){
+//     alert(`its even number`)
+// }
+// else if(userNumber==6){
+//     alert(`its even number`)
+// }
+// else if(userNumber==8){
+//     alert(`its even number`)
+// }
+// else if(userNumber==10){
+//     alert(`its even number`)
+// }
+// else if(userNumber==1){
+//     alert(`its odd number`)
+// }
+// else if(userNumber==3){
+//     alert(`its odd number`)
+// }
+// else if(userNumber==5){
+//     alert(`its odd number`)
+// }
+// else if(userNumber==7){
+//     alert(`its odd number`)
+// }
+// else if(userNumber==9){
+//     alert(`its odd number`)
+// }
+
+// question:10
+// let temp =+prompt("write your city temperature")
+// if(temp>=40){
+//     alert(`It is too hot outside.`)
+// }
+// else if(temp>=30){
+//     alert(`The Weather today is Normal.`)
+// }
+// else if(temp>=20){
+//     alert(`Today Weather is cool.`)
+// }
+// else if(temp>=10){
+//     alert(`OMG! Today’s weather is so Cool.`)
+// }
+
+// question:11
+// let firstNumber =+prompt("write first number")
+// let secondNumber =+prompt("write second number")
+// let operater =prompt("whats  you wanna to do?")
+// if(operater==="+"){
+//     alert(firstNumber+secondNumber)
+// }
+// else if(operater==="-"){
+//     alert(firstNumber-secondNumber)
+// }
+// else if(operater==="*"){
+//     alert(firstNumber*secondNumber)
+// }
+// else if(operater==="/"){
+//     alert(firstNumber/secondNumber)
+// }
+// else if(operater==="%"){
+//     alert(firstNumber%secondNumber)
+// }
+// else{
+//     alert(`you are wrong finding`)
+// }
